0b60a4571508f047618b6c2f999bf76aa8c7583c
Julien Chaperon <julien.chaperon712@gmail.com>
2026-04-25 17:40:12 -0400

    render html page

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Clock backend is running."
+    return render_template("index.html")
 
 def main():
     app.run(debug=True)
